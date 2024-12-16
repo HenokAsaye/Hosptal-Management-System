5e9d63d5e117cd1828ce5ed649aa9a4a4b7dc3a0
Henok Asaye <henokasaye77@gmail.com>
2024-12-17 01:22:22 +0300

    add the cookieparser to  handle the token in the request

--- Backend/src/util/jwt.js
+++ Backend/src/util/jwt.js
@@ -4,10 +4,11 @@ dotenv.config();
 
 export const generateToken = async (Id, res) => {
     const token = jwt.sign(
-        { _id: Id._id, role: Id.role }, 
+        { _id: Id._id, role: Id.role}, 
         process.env.JWT_SECRET
     );
-    res.cookie("token", token, {
+    console.log(token)
+    res.cookie("jwt", token, {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
     });
