d8fbb0012aea5c665dd8d0dec0a4f991d1361cac
Henok Asaye <henokasaye77@gmail.com>
2024-12-17 01:18:32 +0300

    add the verification and reset token feature

---
+++ Backend/src/model/userModel.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+const userSchema = new mongoose.Schema({
+    name: { 
+        type: String, 
+        required: true
+     },
+    email: { type: String,
+         unique: true, 
+         required: true 
+        }, 
+    password: { type: String, 
+        required: true }, 
+    role: {
+      type: String,
+      enum: ["doctor", "nurse", "pharmacist", "receptionist", "lab_technician",],
+      required: true,
+    },
+    isVerified: { type: Boolean, default: false }, 
+    verificationToken:{
+        type:Number
+    },
+    verificationTokenExpiresAt:{
+        type:Date
+    },
+    isVerified:{
+        type:Boolean,
+        default:false
+    },
+    resetToken:{
+        type:String,
+    },
+    resetTokenExpiresAt:{
+        type:Date
+    },
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date, default: Date.now },
+})
+
+
+const User = mongoose.model("User",userSchema);
+export default User;
