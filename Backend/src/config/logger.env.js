7a997267e5797c6992d2129658f80e8c6187d1f5
Henok Asaye <henokasaye77@gmail.com>
2025-01-15 22:33:57 +0300

    modify it for auditlog

--- Backend/src/config/logger.env.js
+++ Backend/src/config/logger.env.js
@@ -1,15 +1,20 @@
 import { createLogger, format, transports } from 'winston';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import fs from 'fs';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
-import fs from 'fs';
 const logDir = path.join(__dirname, 'log');
+
 if (!fs.existsSync(logDir)) {
   fs.mkdirSync(logDir);
 }
+
+const auditFilter = format((info) => {
+  return info.isAudit ? info : false; 
+});
+
 
 export const logger = createLogger({
   level: 'info',
@@ -21,7 +26,12 @@ export const logger = createLogger({
     new transports.File({ filename: path.join(logDir, 'access.log'), level: 'info' }),
     new transports.File({ filename: path.join(logDir, 'combined.log') }),
     new transports.File({ filename: path.join(logDir, 'custom.log'), level: 'warn' }),
-    new transports.File({ filename: path.join(logDir, 'error.log'), level: 'error' })
+    new transports.File({ filename: path.join(logDir, 'error.log'), level: 'error' }),
+    new transports.File({
+      filename: path.join(logDir, 'audit.log'),
+      level: 'info',
+      format: format.combine(auditFilter())
+    })
   ]
 });
 
@@ -32,3 +42,14 @@ if (process.env.NODE_ENV !== 'production') {
     })
   );
 }
+export const logAction = (action, admin, status = 'success', message = '', level = 'info') => {
+  logger.log({
+    level: level,
+    isAudit: true, 
+    timestamp: new Date().toISOString(),
+    action,
+    admin: admin ? admin.username : 'Unknown Admin',
+    status,
+    message
+  });
+};
