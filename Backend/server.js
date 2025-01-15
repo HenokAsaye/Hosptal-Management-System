ca91f19c4b5e41f0fceb59d714c0b768e033e732
Henok Asaye <henokasaye77@gmail.com>
2025-01-15 22:32:18 +0300

    modify to fit for the socket.io requirement

--- Backend/server.js
+++ Backend/server.js
@@ -1,11 +1,20 @@
+import http from "http";
 import app from "./app.js";
 import { connectToDb } from "./src/config/db.config.js";
 import dotenv from "dotenv";
-import {logger} from "./src/config/logger.env.js"
-dotenv.config();
-connectToDb().then(()=>{
-    app.listen(process.env.PORT,()=>{
-        logger.info("server is listening!")
-    })
-}).catch(error=>logger.error("failed to connect to the Db!",error))
+import { logger } from "./src/config/logger.env.js";
+import { initializeSocket } from "./utils/socketService.js";
 
+dotenv.config();
+const server = http.createServer(app);
+initializeSocket(server);
+
+connectToDb()
+  .then(() => {
+    server.listen(process.env.PORT, () => {
+      logger.info("Server is listening!");
+    });
+  })
+  .catch((error) => {
+    logger.error("Failed to connect to the DB!", error);
+  });
